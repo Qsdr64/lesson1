499003a10ee82a62fd30efb68564ba7e602587de
Qsdr64 <nugmanov.valera@mail.ru>
2025-09-04 12:40:46 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -8,8 +8,8 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("http://uitestingplayground.com/dynamicid")
 button_locator = "btn-primary"
-for i in range(3):
-    button = driver.find_element(By.CLASS_NAME, button_locator)
-    button.click()
+button = driver.find_element(By.CLASS_NAME, button_locator)
+button.click()
 sleep(5)
-driver.quit
+
+driver.quit()
