c487d35aa2ac11890cfb1c5c817c7cd3b4685a72
Qsdr64 <nugmanov.valera@mail.ru>
2025-09-16 18:12:42 +0300

    Add files via upload

---
+++ expectation.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+skypro = WebDriverWait(driver, 30)
+skypro.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR,"#award"))
+)
+src = driver.find_element(By.CSS_SELECTOR, "#award")
+print(src.get_attribute("src"))
