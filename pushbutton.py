e92a5a9c2ba6aa6a9997ea16024c835bda7ad026
Qsdr64 <nugmanov.valera@mail.ru>
2025-09-17 18:51:10 +0300

    Update pushbutton.py

--- pushbutton.py
+++ pushbutton.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService())
 driver.implicitly_wait(20)
 
 driver.get("http://uitestingplayground.com/ajax")
