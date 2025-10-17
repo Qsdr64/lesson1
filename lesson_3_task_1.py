b5b49bbacbfe206654295b8b1bb7ec535a78736d
Qsdr64 <nugmanov.valera@mail.ru>
2025-10-17 18:59:16 +0300

    Delete lesson_3_task_1.py

--- lesson_3_task_1.py
+++
@@ -1,6 +0,0 @@
-from user import User
-my_user = User(first_name = input ("Имя:"), last_name = input("Фамилия:"))
-newuser = my_user   
-newuser.yoursfirst_name() 
-newuser.yourslast_name()
-newuser.yourslastname()
