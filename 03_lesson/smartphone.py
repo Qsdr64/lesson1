ebc6140d462ac4f10006dac1a192425f81d70a1d
Qsdr64 <nugmanov.valera@mail.ru>
2025-10-25 01:16:03 +0300

    Delete 03_lesson directory

--- 03_lesson/smartphone.py
+++
@@ -1,6 +0,0 @@
-class Smartphone:
-
-    def __init__ (self, phonebrand,phonemodel,subscribernumber):
-        self.phonebrand = phonebrand
-        self.phonemodel = phonemodel
-        self.subscribernumber = subscribernumber
